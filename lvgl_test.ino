ce6da763983b90682f461f440127a1eef9fdba1c
Chris Keith <chris.keith@gmail.com>
2026-06-08 10:18:05 -0700

    Only need a single cell

--- lvgl_test.ino
+++ lvgl_test.ino
@@ -9,7 +9,7 @@ Arduino_H7_Video  Display(WIDTH, HEIGHT, GigaDisplayShield);
 
 class OLEDWrapper {
   private:
-    lv_obj_t*   leftCell = nullptr;
+    lv_obj_t*   gridCell = nullptr;
     lv_obj_t*   rightCell = nullptr;
     lv_obj_t*   screen = nullptr;
     const int   DEFAULT_FONT_SIZE = 24;
@@ -36,24 +36,20 @@ class OLEDWrapper {
       lv_style_set_line_rounded(line_style, true);
     }
     void setupGrid() {
-      static lv_coord_t col_dsc[] = { 300, 440, LV_GRID_TEMPLATE_LAST };
-      static lv_coord_t row_dsc[] = { 430, 430, LV_GRID_TEMPLATE_LAST };
+      static lv_coord_t col_dsc[] = { WIDTH - 50, LV_GRID_TEMPLATE_LAST };
+      static lv_coord_t row_dsc[] = { HEIGHT - 50, LV_GRID_TEMPLATE_LAST };
 
       lv_obj_t* grid = lv_obj_create(lv_scr_act());
       lv_obj_set_grid_dsc_array(grid, col_dsc, row_dsc);
       lv_obj_set_size(grid, Display.width(), Display.height());
 
-      leftCell = lv_label_create(grid);
-      lv_obj_set_grid_cell(leftCell, LV_GRID_ALIGN_STRETCH, 0, 1,  //column
+      gridCell = lv_label_create(grid);
+      lv_obj_set_grid_cell(gridCell, LV_GRID_ALIGN_STRETCH, 0, 1,  //column
                           LV_GRID_ALIGN_STRETCH, 0, 1);      //row
-      lv_obj_set_style_text_font(leftCell, &lv_font_montserrat_28, 0);
-
-      rightCell = lv_label_create(grid);
-      lv_obj_set_grid_cell(rightCell, LV_GRID_ALIGN_STRETCH, 1, 1,  //column
-                          LV_GRID_ALIGN_STRETCH, 0, 1);      //row
+      lv_obj_set_style_text_font(gridCell, &lv_font_montserrat_28, 0);
     }
     void display(String s, int textSize, uint8_t x, uint8_t y) {
-      lv_label_set_text(leftCell, s.c_str());
+      lv_label_set_text(gridCell, s.c_str());
     }
     void display(String s) {
       display(s, DEFAULT_FONT_SIZE, 10, 10);
